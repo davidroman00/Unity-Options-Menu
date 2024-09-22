a25b94adcbc351eefd5a18be84c8d07a0f4568ca
davidroman00 <davidro3421@gmail.com>
2024-09-22 17:25:31 +0200

    cambio de resolucion funcional

--- Assets/Scripts/MenuButtonsController.cs
+++ Assets/Scripts/MenuButtonsController.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MenuButtonsController : MonoBehaviour
@@ -10,10 +10,35 @@ public class MenuButtonsController : MonoBehaviour
     GameObject _optionsScreen;
     [SerializeField]
     GameObject _mainScreen;
+    [SerializeField]
+    GameObject _dropdown;
+    [SerializeField]
+    GameObject _selectedOption;
+    TextMeshProUGUI _selectedText;
+    void Awake()
+    {
+        _selectedText = _selectedOption.GetComponentInChildren<TextMeshProUGUI>();
+    }
     public void BackButton()
     {
         _optionsScreen.SetActive(false);
         _mainScreen.SetActive(true);
+    }
+    public void ShowHideResolutionDrowpdown()
+    {
+        _dropdown.SetActive(!_dropdown.activeInHierarchy);
+    }
+    public void Select1920x1080Option()
+    {
+        _selectedText.text = "1920x1080";
+        _dropdown.SetActive(false);
+        Screen.SetResolution(1920, 1080, true);
+    }
+    public void Select1600x900Option()
+    {
+        _selectedText.text = "1600x900";
+        _dropdown.SetActive(false);
+        Screen.SetResolution(1600, 900, false);
     }
     public void OptionsButton()
     {
